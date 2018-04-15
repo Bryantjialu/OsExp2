765dd07e32e9df9cc6a9250e9c3a09f307619272
bryantjialu <847124185@qq.com>
2018-04-15 22:25:47 +0800

    eleventh

--- seven_read.cpp
+++ seven_read.cpp
@@ -1,94 +1,58 @@
-//customer.c  
-#include <stdio.h>    
-#include <sys/ipc.h>    
-#include <sys/shm.h>    
-#include <semaphore.h>    
-#include <fcntl.h>    
-#include <stdlib.h>    
-#include <unistd.h>    
-#include <string.h>  
-  
-#include "shm_com_sem.h"  
-  
-int main(void)  
-{  
-    void * shared_memory=(void *)0;  
-    struct shared_mem_st *shared_stuff;  
-  
-    int stmid;  
-    int num_read;  
-    pid_t fork_result;  
-    sem_t *sem_queue,*sem_queue_empty,*sem_queue_full;  
-      
-    stmid=shmget((key_t)1234,sizeof(struct shared_mem_st),0666|IPC_CREAT);//获得已创建共享内存  
-     if(stmid==-1)  
-    {  
-      perror("shmget failed");  
-      exit(1);  
-    }  
-    if((shared_memory = shmat(stmid,0,0))<(void *)0){  //若共享内存区映射到本进程的进程空间失败  
-        perror("shmat failed");    
-        exit(1);    
-    }   
-  
-    shared_stuff=(struct shared_mem_st *)shared_memory;  
-  
-    //获取三个信号量  
-    sem_queue=sem_open("queue_mutex",0);  
-    sem_queue_empty=sem_open("queue_empty",0);  
-    sem_queue_full=sem_open("queue_full",0);  
-  
-  
-    //创建两个进程  
-    fork_result=fork();  
-    if(fork_result==-1)  
-        fprintf(stderr,"fork failed\n");  
-    int running=1;  
-    if(fork_result==0)//子进程  
-    {  
-        while(running)  
-        {  
-            sem_wait(sem_queue_full);  
-            sem_wait(sem_queue);//等待信号量  
-            sleep(2);  
-            printf("child pid is %d,you wrote:%s\n",getpid(),shared_stuff->buffer[shared_stuff->line_read]);//输出进程号和消息内容  
-              
-              
-            if(strncmp(shared_stuff->buffer[shared_stuff->line_read],"end",3)==0)//如果为end则退出  
-            {   running=0;  
-            }     
-        //  printf("%d",running);  
-            shared_stuff->line_read=(shared_stuff->line_read+1)%NUM_LINE;//读指针改变  
-            sem_post(sem_queue);//发送信号量  
-            sem_post(sem_queue_empty);  
-        }  
-        sem_unlink(queue_mutex);  
-        sem_unlink(queue_empty);  
-        sem_unlink(queue_full);  
-    }  
-    else//父进程  
-    {   while(running)  
-        {     
-            sem_wait(sem_queue_full);  
-            sem_wait(sem_queue);//等待信号量  
-            sleep(2);  
-            printf("parent pid is %d,you wrote:%s\n",getpid(),shared_stuff->buffer[shared_stuff->line_read]);//输出进程号和消息内容  
-              
-            if(strncmp(shared_stuff->buffer[shared_stuff->line_read],"end",3)==0)//如果为end则退出  
-            {  
-             running=0;  
-            }  
-        //  printf("%d",running);  
-            shared_stuff->line_read=(shared_stuff->line_read+1)%NUM_LINE;//读指针改变  
-            sem_post(sem_queue);//发送信号量  
-            sem_post(sem_queue_empty);  
-                  
-        }  
-     
-        sem_unlink(queue_mutex);  
-        sem_unlink(queue_empty);  
-        sem_unlink(queue_full);  
-    }  
-    waitpid(fork_result,NULL,0);  
-    exit(EXIT_SUCCESS);  
-}  
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/shm.h>
+#include<errno.h>
+#include<semaphore.h>
+#include<string.h>
+#include<fcntl.h>
+#define BUF_SIZE 1024
+
+int main(int argc,char *argv[])
+{
+	if(argc!=2)
+	{
+		printf("invaild arguement\n");
+		exit(1);
+	}
+	int shm_id = atoi(argv[1]);
+	char *shm_ptr;	
+	if((shm_ptr=shmat(shm_id,0,0))==(void*)-1)
+	{
+		printf("error in shmat\n");
+		exit(1);
+	}
+	sem_t *mutex = sem_open("mutex",O_CREAT);
+	sem_t *full  = sem_open("full",O_CREAT);
+	sem_t *empty = sem_open("empty",O_CREAT);
+
+	pid_t pid;
+	if((pid=fork())<0)
+	{
+		printf("error in fork()\n");
+		exit(1);
+	}
+	else if(pid>0)
+	{
+		while(1)
+		{
+			sem_wait(full);
+			sem_wait(mutex);
+			printf("process %d receive message :%s\n",getpid(),shm_ptr);
+			strcpy(shm_ptr,"");
+			sem_post(mutex);
+			sem_post(empty);
+		}	
+	}
+	else
+	{
+		while(1)
+		{
+			sem_wait(full);
+			sem_wait(mutex);
+			printf("process %d receive message :%s\n",getpid(),shm_ptr);
+			strcpy(shm_ptr,"");	
+			sem_post(mutex);
+			sem_post(empty);
+		}
+	}	
+}
--- seven_write.cpp
+++ seven_write.cpp
@@ -1,104 +1,56 @@
-//头文件shm_com_sem.h  
-#include <fcntl.h>   
-#include <sys/stat.h>  
-#include <semaphore.h>    
-  
-#define LINE_SIZE 256  
-#define NUM_LINE 16  
-  
-const char * queue_mutex="queue_mutex";  
-const char * queue_empty="queue_empty";  
-const char * queue_full="queue_full";  
-//公用缓冲区    
-struct shared_mem_st  
-{  
-    char buffer[NUM_LINE][LINE_SIZE];//缓冲数据组  
-    int line_write;//读写指针  
-    int line_read;  
-};  
-  
-  
-//producer.c  
-  
-#include <stdio.h>    
-#include <sys/ipc.h>    
-#include <sys/shm.h>    
-#include <semaphore.h>    
-#include <fcntl.h>    
-#include <stdlib.h>    
-#include <unistd.h>    
-#include <string.h>  
-  
-#include "shm_com_sem.h"  
-  
-  
-int main(void)  
-{  
-    void *shared_memory=(void *)0;//共享内存 缓冲区指针  
-    struct shared_mem_st *shared_stuff;  
-    char key_line[256];  
-  
-    int stmid;//共享内存id  
-  
-    sem_t *sem_queue,*sem_queue_empty,*sem_queue_full;//访问共享内存的信号量指针  
-  
-  
-    stmid=shmget((key_t)1234,sizeof(struct shared_mem_st),0666|IPC_CREAT);//创建一个新的共享内存  
-     if(stmid==-1)  
-    {  
-      perror("shmget failed");  
-      exit(1);  
-    }  
-    if((shared_memory = shmat(stmid,0,0))<(void *)0){  //若共享内存区映射到本进程的进程空间失败  
-        perror("shmat failed");    
-        exit(1);    
-    }   
-  
-    shared_stuff=(struct shared_mem_st *)shared_memory;//将缓冲区指针转换为shared_mem_st格式  
-  
-  
-    sem_queue=sem_open("queue_mutex",O_CREAT,0644,1);  
-    sem_queue_empty=sem_open("queue_empty",O_CREAT,0644,1);  
-    sem_queue_full=sem_open("queue_full",O_CREAT,0644,1);//创建三个信号量  
-  
-  
-    //读写指针初始化  
-    shared_stuff->line_write=0;  
-    shared_stuff->line_read=0;  
-  
-    char b[BUFSIZ];  
-    int running=1;  
-    while(running)  
-    {  
-        sleep(1);  
-        sem_wait(sem_queue_empty);  
-        sem_wait(sem_queue);//等待信号量输入  
-        printf("Enter some text:");  
-        fgets(b,BUFSIZ,stdin);  
-        strncpy(shared_stuff->buffer[shared_stuff->line_write],b,LINE_SIZE);  
-        shared_stuff->line_write=(shared_stuff->line_write+1)%NUM_LINE;//写指针增加  
-        if(strncmp(b,"end",3)==0)//如果输入为end则退出  
-        {  
-             running=0;  
-         }  
-        sem_post(sem_queue);//发送信号量  
-        sem_post(sem_queue_full);  
-    }  
-//为了防止结束后进程阻塞，所以再发送一次信号量和内容  
-    sem_post(sem_queue);//发送信号量  
-    sem_post(sem_queue_full);  
-    strncpy(shared_stuff->buffer[shared_stuff->line_write],b,LINE_SIZE);  
-    shared_stuff->line_write=(shared_stuff->line_write+1)%NUM_LINE;//写指针增加  
-  
-    if(shmdt(shared_memory)==-1)//解除映射  
-    {  
-        fprintf(stderr,"shmdt failed\n");  
-        exit(EXIT_FAILURE);  
-    }  
-    //删除共享内存区  
-    if(shmctl(stmid,IPC_RMID,0)==-1)  
-    {  
-        fprintf(stderr,"shmctl failed\n");  
-        exit(EXIT_FAILURE);  
-    }  
-}  
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<fcntl.h>
+#include<string.h>
+#include<semaphore.h>
+
+#define BUF_SIZE 1024
+
+#define NBUFF 1
+
+int main(void )
+{
+	int shm_id ;
+	char msg[BUF_SIZE];
+	if((shm_id=shmget(IPC_PRIVATE,BUF_SIZE,0666))<0)
+	{
+		printf("error in shmget");
+		exit(1);
+	}	
+	printf("writer,shm id is :%d\n",shm_id);
+	char *shm_ptr;
+	if((shm_ptr=shmat(shm_id,0,0))==(void*)-1)
+	{
+		printf("error in shmat");
+		exit(1);
+	}
+	sem_unlink("mutex");
+	sem_unlink("full");
+	sem_unlink("empty");
+	sem_t *mutex = sem_open("mutex",O_CREAT,0666,1);
+	sem_t *full  = sem_open("full",O_CREAT,0666,0);
+	sem_t *empty = sem_open("empty",O_CREAT,0666,NBUFF);
+	while(1)
+	{
+		printf("writer,pls enter the string:  \n");
+		if((fgets(msg,BUF_SIZE-1,stdin))==NULL)
+		{
+			printf("error in fgets\n");
+			exit(1);
+		}
+		
+		sem_wait(empty);
+		sem_wait(mutex);	
+		strcpy(shm_ptr,msg);
+		printf("writer,write :%s\n",shm_ptr);
+		sem_post(mutex);
+		sem_post(full);
+		if(strncmp(msg,"//end",5)==0)
+		{
+			break;
+		}
+	}	
+	return 0;
+}
